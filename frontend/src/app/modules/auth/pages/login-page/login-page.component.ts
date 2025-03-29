7985ca15a2b06cfaf247feafcf43bb788fa3a139
franklindrw <franklindrw@gmail.com>
2025-03-29 17:11:40 -0300

    feat: adicionar link para cadastro e recuperação de senha na página de login

--- frontend/src/app/modules/auth/pages/login-page/login-page.component.ts
+++ frontend/src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from "../../components/login-form/login-form.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, RouterModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
