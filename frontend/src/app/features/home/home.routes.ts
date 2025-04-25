4c82335f3be3188e3da6d0d24666cf9395036ea5
franklindrw <franklindrw@gmail.com>
2025-04-25 15:17:45 -0300

    fix[front]: ajustando o provider

--- frontend/src/app/features/home/home.routes.ts
+++ frontend/src/app/features/home/home.routes.ts
@@ -1,10 +1,11 @@
 import { AuthUseCase } from "@core/use-cases/auth.use-case";
 import { HomePageComponent } from "./page/home-page/home-page.component";
+import { AuthService } from "@features/auth/services/auth.service";
 
 export const HOME_ROUTES = [
   {
     path: '',
     component: HomePageComponent,
-    providers: [AuthUseCase]
+    providers: [AuthUseCase, AuthService]
   }
 ]
