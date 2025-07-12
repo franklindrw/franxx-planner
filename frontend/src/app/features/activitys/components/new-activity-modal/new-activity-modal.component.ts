4eb45aedb3dc6f30bbea0d1d0f377eba34e70673
franklindrw <franklindrw@gmail.com>
2025-07-12 11:30:58 -0300

    feat: iniciando modal de nova atividades

---
+++ frontend/src/app/features/activitys/components/new-activity-modal/new-activity-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-activity-modal',
+  imports: [],
+  templateUrl: './new-activity-modal.component.html',
+  styleUrl: './new-activity-modal.component.scss'
+})
+export class NewActivityModalComponent {
+
+}
